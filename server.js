f3dfa15a13c2b3bbd02ea39937762f2aa86c94b1
Majd Hailat <majdhailat64@gmail.com>
2020-10-12 15:21:39 -0400

    *

--- server.js
+++ server.js
@@ -215,6 +215,6 @@ app.post("/user/changepassword", function(req, res) {
   }
 })
 
-app.listen(5000, function() {
+app.listen(process.env.PORT || 5000, function() {
   console.log("Server started.");
 });
