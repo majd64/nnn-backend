373b871f6785c3ea2e38ae8331cf3f0d0452f44b
Majd Hailat <majdhailat64@gmail.com>
2020-10-11 23:14:14 -0400

    *

--- server.js
+++ server.js
@@ -22,10 +22,9 @@ mongoose.set("useCreateIndex", true);
 app.set('trust proxy', 1) // trust first proxy
 
 app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://nnn-server.herokuapp.com');
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', "http://localhost:3000");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
